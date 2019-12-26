31046b0c8366f13367dd398065b83f80c9e76614
Florent Peterschmitt <florent@peterschmitt.fr>
2019-12-26 14:24:27 +0100

    use WaitGroup from crossbeam

--- robot/src/main.rs
+++ robot/src/main.rs
@@ -1,40 +1,42 @@
 use crossbeam::crossbeam_channel::unbounded;
+use crossbeam::sync::WaitGroup;
 use robot::client::mattermost::Mattermost;
 use robot::client::*;
 use robot::conf::Conf;
 use robot::handlers;
 use robot::instance::Instance;
 use robot::middleware;
+use std::thread;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cfg = Conf::new()?;
-    println!("{:?}", cfg);
 
     let (sender, receiver) = unbounded();
+    let wg = WaitGroup::new();
 
-    let mut threads = vec![];
-    for _i in 0..2 {
+    for _i in 0..4 {
         let mrecv = receiver.clone();
         let mcfg = cfg.clone();
-        let inst = std::thread::spawn(move || {
+        let wg = wg.clone();
+        thread::spawn(move || {
             let client = Mattermost::new(mcfg);
             Instance::new(&client)
                 .add_middleware(Box::new(middleware::Debug::new("middleware 1")))
                 .add_post_handler(Box::new(handlers::Debug::new("post handler")))
                 .run(mrecv);
+            drop(wg);
         });
-        threads.push(inst);
     }
 
-    let listener = std::thread::spawn(move || {
-        Mattermost::new(cfg).listen(sender);
-    });
-
-    threads.push(listener);
-
-    for i in threads {
-        let _ = i.join();
+    {
+        let wg = wg.clone();
+        thread::spawn(move || {
+            Mattermost::new(cfg).listen(sender);
+            drop(wg);
+        });
     }
+
+    wg.wait();
 
     Ok(())
 }
